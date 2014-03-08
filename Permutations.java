815f501c1d56db1501128f630d7c9be62e5439d4
swiftwolff <jeff.hsu@west.cmu.edu>
2014-03-08 10:39:17 -0800

    new solutions

--- Permutations.java
+++ Permutations.java
@@ -3,51 +3,51 @@ import java.util.ArrayList;
 public class Permutations{
 	
 	public static ArrayList<ArrayList<Integer>> permute(int[] num) {
-		ArrayList<ArrayList<Integer>> all = new ArrayList<ArrayList<Integer>>();
-		if (num.length==0||num.length==1){
-			return all;
-		}
-		
-		if (num.length==1){
-			all.add(saveToArray(num));
-			return all;
-		}
-		
-		build(num,0,num.length,all);
-		return all;
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        if (num.length==0){
+            ArrayList<Integer> tmp = new ArrayList<Integer>();
+            res.add(tmp);
+            return res;
+        }
+        if (num.length==1){
+            res.add(arryToLst(num));
+            return res;
+        }
+        build(num,0,num.length,res);
+        return res;
     }
-	
-	public static void build(int[] num, int k, int n,ArrayList<ArrayList<Integer>> all){
-		if (k==n){
-			all.add(saveToArray(num));
-		}
-		
-		for (int i=k;i<n;i++){
-			swap(num,i,k);
-			build(num,k+1,n,all);
-			swap(num,i,k);
-		}
-		
-	}
-	
-	public static void swap(int[] A,int i, int j){
-		int tmp = A[i];
-		A[i] = A[j];
-		A[j] = tmp;
-	}
-	
-	public static ArrayList<Integer> saveToArray(int[] num){
-		ArrayList<Integer> res = new ArrayList<Integer>();
-		for (int i=0;i<num.length;i++){
-			res.add(num[i]);
-		}
-		return res;
-	}
+    
+    public static void build(int[] num, int k, int n, ArrayList<ArrayList<Integer>> res){
+        if (k==n){
+            res.add(arryToLst(num));
+        }
+        
+        for (int i=k;i<num.length;i++){
+            swap(num,i,k);
+            build(num,k+1,n,res);
+            swap(num,i,k);
+        }
+        
+    }
+    
+    public static void swap(int[] num, int i, int j){
+        int tmp = num[i];
+        num[i] = num[j];
+        num[j] = tmp;
+    }
+    
+    public static ArrayList<Integer> arryToLst(int[] num){
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        for (int i=0;i<num.length;i++){
+            res.add(num[i]);
+        }
+        return res;
+    }
 
 	public static void main(String args[]){
 		int[] A = {1,2,3,4};
 //		permute(A);
-		System.out.println(permute(A).size());
+		System.out.println(permute(A));
 		
 	}
 }
