a7008d39c418c699fcd92af8a1cabce65f96e316
swiftwolff <jeff.hsu@west.cmu.edu>
2014-04-19 13:57:45 -0700

    new solution

--- ConstructPreorderAndInorderTraversal.java
+++ ConstructPreorderAndInorderTraversal.java
@@ -4,16 +4,19 @@ import java.util.HashMap;
 public class ConstructPreorderAndInorderTraversal {
 	
 	public static TreeNodeI buildTree(int[] preorder, int[] inorder) {
+		if(preorder.length==0||inorder.length==0){
+			return null;
+		}
         HashMap<Integer,Integer> inorderTable = new HashMap<Integer,Integer>();
         for(int i=0;i<inorder.length;i++){
         	inorderTable.put(inorder[i],i);
         }
         
-        TreeNodeI root = build(inorderTable,preorder,0,preorder.length-1,inorder,0,inorder.length-1);
+        TreeNodeI root = build(inorderTable,preorder,0,inorder,0,inorder.length-1);
 		return root;
     }
 	
-	public static TreeNodeI build(HashMap<Integer,Integer> inorderTable, int[] preorder,int pcur, int pend,int[] inorder, int istart, int iend){
+	public static TreeNodeI build(HashMap<Integer,Integer> inorderTable, int[] preorder,int pcur, int[] inorder, int istart, int iend){
 		
 		if(istart>iend){
 			return null;
@@ -22,8 +25,8 @@ public class ConstructPreorderAndInorderTraversal {
 		int end = inorderTable.get(preorder[pcur]);
 		
 		int len = end-istart;  //WHY WHY WHY WHY!!!!!!
-		root.left = build(inorderTable,preorder,pcur+1,pcur+1+len-1,inorder,istart,end-1);  //what is pcur here
-		root.right = build(inorderTable,preorder,pcur+1+len-1+1,pend,inorder,end+1,iend);
+		root.left = build(inorderTable,preorder,pcur+1,inorder,istart,end-1);  //what is pcur here
+		root.right = build(inorderTable,preorder,pcur+len+1,inorder,end+1,iend);
 		
 		return root;
 	}
@@ -33,6 +36,9 @@ public class ConstructPreorderAndInorderTraversal {
 	public static void main(String args[]){
 		int[] preorder = {7,10,4,3,1,2,8,11};
 		int[] inorder = {4,10,3,1,7,11,8,2};
+		
+		
+		
 		
 		TreeNodeI res = buildTree(preorder, inorder);
 		res.preorderPrint(res);
