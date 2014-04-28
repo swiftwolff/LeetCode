6210d75cb9a4ade4b2c1a23e8b36b3ec92f7593c
swiftwolff <jeff.hsu@west.cmu.edu>
2014-04-28 16:51:57 -0700

    new solution

---
+++ LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+
+public class LongestSubstringWithoutRepeatingCharacters {
+	
+	public int lengthOfLongestSubstring(String s) {
+        
+		if(s==null||s.length()==0){
+			return 0;
+		}
+		boolean[] alphebat = new boolean[256];
+		Arrays.fill(alphebat, false);
+		int count = 0;
+		int ans = Integer.MIN_VALUE;
+		int i = 0;
+		int j = 0;
+		while(j<s.length()){
+			if(alphebat[s.charAt(j)]){
+				ans = Math.max(ans, count);
+				while(alphebat[s.charAt(j)]){
+					alphebat[s.charAt(i)] = false;
+					count--;
+					i++;
+				}
+				
+//			if(i==j-1){
+//				alphebat[s.charAt(i)] = false;  //didn't clean
+//				ans = Math.max(ans, count);
+//				i = j;
+//				count=0;
+//			}
+			}else{
+				alphebat[s.charAt(j)] = true;
+				j++;
+				count++;
+			}
+			ans = Math.max(ans, count);
+		}
+		
+		return ans;
+    }
+	
+	
+	
+	
+	public static void main(String args[]){
+		String s = "hnwnkuewhsqmgbbuqcljjivswmdkqtbxixmvtrrbljptnsnfwzqfjmafadrrwsofsbcnuvqhffbsaqxwpqcac";
+//		System.out.println(s.length());
+		LongestSubstringWithoutRepeatingCharacters solution = new LongestSubstringWithoutRepeatingCharacters();
+		System.out.println(solution.lengthOfLongestSubstring(s));
+//		boolean[] table = new boolean[256];
+//		table['a'] = true;
+//		for(int i=0;i<256;i++){
+//			if(table[i]==true){
+//				System.out.println(i);
+//			}
+//		}
+//		System.out.println(table[97]);
+	}
+}
--- ZigZagConvertion.java
+++ ZigZagConvertion.java
@@ -1,66 +1,85 @@
+import java.util.Arrays;
+
 
 public class ZigZagConvertion {
 	
-	public String convert(String s, int nRows) {
-		if(s==null||s.length()==0){
-			return "";
-		}
-        if(nRows<=1){
-            return s;
-        }
-        int len = s.length();
-        int count = 0;
-        int top = len;
-        int n = nRows-2;
-        while(top > 0){
-            top-=nRows;
-            count++;
-            while(n>0 && top >0){
-                top-=1;
-                count++;
-                n--;
-            }
-            n = nRows-2;
-           
-        }
-        char[][] table = new char[nRows][count];
-        int negrow = 0;
-        int cur = 0;
-        int offset = nRows-2;
-        for(int j=0;j<count;j++){ //col moves up
-        	while( j % (offset+1) != 0 && cur<len){
-        		table[negrow-1][j] = s.charAt(cur);
-        		negrow--;
-        		cur++;
-        		j++;
-        	}     	
-        	if(cur>len){
-    			break;
-    		}
-        	for(int i=0;i<nRows&&cur<len;i++){ //rows alter 
-        		table[i][j] = s.charAt(cur);
-        		cur++;
-        		
-        		negrow = i;
-        	}
-        }
-        
-        StringBuffer sb = new StringBuffer();
-        for(int i=0;i<nRows;i++){
-        	for(int j=0;j<count;j++){
-        		if(table[i][j]!=' '){
-        			sb.append(table[i][j]);
-        		}
-        	}
-        }
-        
-        return sb.toString();
-        
-    }
-	
+//	public String convert(String s, int nRows) {
+//		if(s==null||s.length()==0){
+//			return "";
+//		}
+//        if(nRows<=1){
+//            return s;
+//        }
+//        int len = s.length();
+//        int count = 0;
+//        int top = len;
+//        int n = nRows-2;
+//        while(top > 0){
+//            top-=nRows;
+//            count++;
+//            while(n>0 && top >0){
+//                top-=1;
+//                count++;
+//                n--;
+//            }
+//            n = nRows-2;
+//           
+//        }
+//        char[][] table = new char[nRows][count];
+//        int negrow = 0;
+//        int cur = 0;
+//        int offset = nRows-2;
+//        for(int j=0;j<count;j++){ //col moves up
+//        	while( j % (offset+1) != 0 && cur<len){
+//        		table[negrow-1][j] = s.charAt(cur);
+//        		negrow--;
+//        		cur++;
+//        		j++;
+//        	}     	
+//        	if(cur>len){
+//    			break;
+//    		}
+//        	for(int i=0;i<nRows&&cur<len;i++){ //rows alter 
+//        		table[i][j] = s.charAt(cur);
+//        		cur++;
+//        		
+//        		negrow = i;
+//        	}
+//        }
+//        
+//        StringBuffer sb = new StringBuffer();
+//        for(int i=0;i<nRows;i++){
+//        	for(int j=0;j<count;j++){
+//        		if(table[i][j]!=' '){
+//        			sb.append(table[i][j]);
+//        		}
+//        	}
+//        }
+//        
+//        return sb.toString();
+//        
+//    }
+	public String convert(String s,  int  nRows) {  
+        if (nRows<= 1  || s.length()< 2 ){  
+            return  s;  
+        }  
+        String[] ss =  new  String[nRows];   
+        Arrays.fill(ss,  "" );  
+        int  nGroup =  2  * (nRows- 1 );  
+          
+        for ( int  i= 0 ; i<s.length(); i++){  
+            ss[nRows- 1 -Math.abs(nRows- 1 -(i%nGroup))] += s.charAt(i);  
+        }  
+          
+        String ret =  "" ;  
+        for (String str : ss){  
+            ret += str;  
+        }  
+        return  ret;  
+    }  
 	
 	public static void main(String args[]){
 		ZigZagConvertion solution = new ZigZagConvertion();
-		System.out.println(solution.convert("ABCDE",4));
+		System.out.println(solution.convert("paypalishiring",3));
 	}
 }
