ed9129e5841989fd294d1ffaac5711512451556a
swiftwolff <jeff.hsu@west.cmu.edu>
2014-05-09 18:19:35 -0700

    new solution

--- SiftScience.java
+++ SiftScience.java
@@ -5,28 +5,24 @@ public class SiftScience {
         // Implement here
         int total = 0;
         String[] processPlus = expr.split("\\+");  //this gives you [4x12, 4 ,9x5x2,9x0];
-
         ArrayList<Integer> plusInteger = new ArrayList<Integer>();
-        int sum = 1;
         for(int i=0;i<processPlus.length;i++){
-        	System.out.println(processPlus[i]);
-        	
             String[] processMult = processPlus[i].split("x");
-
+            int sum = 1;
             for(int j=0;j<processMult.length;j++){  //this gives you [4, 12] ,[4], [9,5,2], [9,0];
-                System.out.println(processMult[j]);
-                // processMult[j] == "4"        ["4"] -> 1
-//                if(processMult[j].length()<2){
-//                    plusInteger.add(Integer.parseInt(processMult[j]));
-//                    continue;
-//                }
-//                System.out.println(sum);
-//                sum*= Integer.parseInt(processMult[j]);
-//                plusInteger.add(sum);
+//            	System.out.println(processMult[j]);
+            	 if(processMult.length<2){
+                   plusInteger.add(Integer.parseInt(processMult[j]));
+                   continue;
+               }
+            	sum*=Integer.parseInt(processMult[j]);
             }
+            if(sum==1){
+            	sum=0;
+            }
+            plusInteger.add(sum);
         
         }
-        
         for(int k:plusInteger){
             total+=k;
         }
