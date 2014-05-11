e83e1d25812f38aed9c2d07dda600c00a7a4261d
swiftwolff <jeff.hsu@west.cmu.edu>
2014-05-10 17:52:31 -0700

    new solution

---
+++ TrappingRainWater.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+
+public class TrappingRainWater {
+	public int trap(int[] A) {
+        //corner case
+        if(A.length==0){
+            return 0;
+        }
+        //find out the max state from left to right
+        int[] left = new int[A.length];
+        //max state from right to left
+        int[] right = new int[A.length];
+        System.out.println(Arrays.toString(A));
+        left[0] = A[0];
+        for(int i=1;i<A.length;i++){
+            left[i] = Math.max(left[i-1],A[i]);
+        }
+        right[A.length-1] = A[A.length-1];
+        for(int i=A.length-2;i>-1;i--){
+            right[i] = Math.max(right[i+1],A[i]);
+        }
+        System.out.println(Arrays.toString(left));
+        System.out.println(Arrays.toString(right));
+        int sum = 0;
+        for(int i=1;i<A.length-1;i++){
+            sum+= Math.min(left[i],right[i])-A[i];
+        }
+        return sum;
+    }
+	public static void main(String args[]){
+		int[] A = {2,1,0,2};
+		TrappingRainWater solution = new TrappingRainWater();
+		System.out.println(solution.trap(A));
+	}
+  
+}
