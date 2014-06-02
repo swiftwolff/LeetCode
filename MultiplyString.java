9ad961fc5299c2cd91cedd9126e4494e196aaab8
swiftwolff <jeff.hsu@west.cmu.edu>
2014-06-02 16:55:20 -0700

    new solution

---
+++ MultiplyString.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+
+public class MultiplyString {
+	
+	public static String multiply(String num1, String num2){
+		
+		if(num1==null||num2==null) return null;
+		if(num1.equals("0")||num2.equals("0")) return "0";
+		
+		int[] resChar = new int[(num1.length()+num2.length())*2];
+		int cur = resChar.length-1;
+		int carry = 0;
+		int inc = 0;
+		for(int i=num2.length()-1;i>=0;i--){
+			int b = num2.charAt(i) - '0';
+			cur = cur - inc;
+			for(int j=num1.length()-1;j>=0;j--){
+				int a = num1.charAt(j)-'0';
+				resChar[cur]+=a*b;
+				carry = resChar[cur]/10;
+				resChar[cur]%=10;
+				resChar[cur-1]+=carry;
+				cur--;
+			}
+			inc++;
+			cur = resChar.length-1;
+		}
+		StringBuilder sb = new StringBuilder();
+		for(int i=0;i<resChar.length;i++){
+			if(!(resChar[i]==0)){
+				while(i<resChar.length){
+					sb.append(resChar[i]);
+					i++;
+				}
+				break;
+			}
+		}
+		
+		return sb.toString();
+	}
+	
+	public static void main(String args[]){
+		String num1 = "1245";
+		String num2 = "139";
+		System.out.println(multiply(num1,num2));
+	}
+}
--- RestoreIPAddress.java
+++ RestoreIPAddress.java
@@ -4,15 +4,18 @@ import java.util.List;
 
 public class RestoreIPAddress {
 	
-	public static List<String> restoreIpAddress(String s){
+	public static List<String> restoreIpAddresses(String s){
 		ArrayList<String> res = new ArrayList<String>();
-		if(s.length()==0||s==null) return null;
+		if(s.length()==0||s==null) return res;
 		restoreHelper(s,0,s.length(),9,3,"",res);
 		return res;
 	}
 	
 	public static void restoreHelper(String s,int curIndex, int total, int upper, int lower, String curStr, ArrayList<String> res){
 		if(upper==0){
+			String tmpStr = s.substring(curIndex,s.length());
+			if(Integer.valueOf(tmpStr)>255) return;
+			if(tmpStr.charAt(0)=='0'&&tmpStr.length()>1) return;
 			curStr = curStr + s.substring(curIndex,s.length());
 			res.add(curStr);
 			return;
@@ -23,6 +26,7 @@ public class RestoreIPAddress {
 			if(second > upper || second < lower) continue;
 			String tmpStr = s.substring(curIndex,i);
 			if(Integer.valueOf(tmpStr)>255) continue;
+			if(tmpStr.charAt(0)=='0'&&tmpStr.length()>1) continue;
 			String curStrClone = new String(curStr);
 			curStrClone = curStr+tmpStr+".";
 			restoreHelper(s,i,total,upper-3,lower-1,curStrClone,res);
@@ -32,8 +36,8 @@ public class RestoreIPAddress {
 	
 	public static void main(String args[]){
 		
-		String s = "25525511135";
-		System.out.println(restoreIpAddress(s));
+		String s = "172162541";
+		System.out.println(restoreIpAddresses(s));
 		
 		
 	}
